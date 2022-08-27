3eeb816a9c3bb44c79647723ace7c8745a6caabf
Sofia Lacosta <sofi.lacosta@hotmail.com>
2022-08-27 12:47:58 -0300

    NavBar

---
+++ src/components/NavVar.js
@@ -0,0 +1,10 @@
+const NavBar = () => {
+    return <div>
+        <ul>
+            <li>Inicio</li>
+            <li>Galeria</li>
+            <li>Contactos</li>            
+        </ul>
+    </div>;
+};
+export default NavBar;
