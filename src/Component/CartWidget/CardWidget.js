2f72395441170b22a84c650551b7b37a83af5d05
Sofia Lacosta <sofi.lacosta@hotmail.com>
2022-09-27 13:05:27 -0300

    estiloCarrito

--- src/Component/CartWidget/CardWidget.js
+++ src/Component/CartWidget/CardWidget.js
@@ -1,14 +1,23 @@
 import carrito from '../../Carrito.png'
 import './style.css'
 import { NavLink } from "react-router-dom"
+import { useContext } from "react"
+import { CartContext } from '../../context/CartContext';
 
 const CardWidget = () => {
-  return (
+  const {cart} = useContext(CartContext);
+  return (<div className='carritoo'>
     <NavLink to={"/cart"}>
       <img src={carrito} className="carrito" alt="logo" />
     </NavLink>
+    <h1 className='numeroCarrito'>{cart.length}</h1>
+  </div>
+    
     
   )
 }
 
 export default CardWidget
+
+
+
